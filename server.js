16567c44ce37cf89551bc5129cc7b49fc64bb74f
Raymond <uchennaraymond74@gmail.com>
2025-06-03 13:42:55 +0100

    added server.js back

---
+++ server.js
@@ -0,0 +1,20 @@
+import http from "http"
+import app from "./app/app.js"
+
+
+const PORT = 4000
+
+
+const server =http.createServer(app)
+
+// app.get("/",(req,res)=>{
+//     res.send("hola")
+// })
+
+
+server.listen(PORT,()=>{
+    console.log("🚀 Sever currently running on port 4000");
+    
+})
+
+
